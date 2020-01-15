154cec9b4995da2076540f282383ab836768d6cc
Guido <hi@guido.nyc>
2020-01-15 13:47:43 -0500

    start separating into components

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -46,20 +46,18 @@ class Line extends React.Component {
 class Pairing extends React.Component {
   constructor(props) {
     super(props)
-    const { sport } = props
-    const { pairings } = sport
-
-    this.constructorEdges = {}
-    _.each(pairings, (constructorIds, year) => {
-      this.constructorEdges[year] = {}
-
-      _.each(constructorIds, (drivers, constructorId) => {
-        this.constructorEdges[year][constructorId] = React.createRef()
-      })
-    })
 
     this.state = {
       rendered: false,
+    }
+
+    this.updatePairings = this.updatePairings.bind(this)
+    this.updatePairings()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.sport !== this.props.sport) {
+      this.updatePairings()
     }
   }
 
@@ -69,11 +67,44 @@ class Pairing extends React.Component {
     })
   }
 
+  updatePairings() {
+    this.constructorEdges = {}
+    this.driverEdges = {}
+
+    const { constructorEdges, driverEdges, props } = this
+    const { sport } = props
+    const { pairings } = sport
+
+    _.each(pairings, (constructorIds, year) => {
+      _.each(constructorIds, (driverIds, constructorId) => {
+        if (!constructorEdges[constructorId]) {
+          constructorEdges[constructorId] = {}
+        }
+
+        constructorEdges[constructorId][year] = React.createRef()
+
+        _.each(driverIds, driverId => {
+          if (!driverEdges[driverId]) {
+            driverEdges[driverId] = {}
+          }
+
+          if (!driverEdges[driverId][year]) {
+            driverEdges[driverId][year] = {}
+          }
+
+          driverEdges[driverId][year][constructorId] = React.createRef()
+        })
+      })
+    })
+  }
+
   render() {
-    const { props, state, constructorEdges } = this
+    const { props, state, constructorEdges, driverEdges } = this
     const { sport } = props
     const { rendered } = state
     const { pairings, constructors, drivers } = sport
+
+    console.log(driverEdges)
 
     return (
       <div className={css.container}>
@@ -86,39 +117,106 @@ class Pairing extends React.Component {
           return (
             <div className={css.yearContainer} key={year}>
               <div>{year}</div>
-              {_.map(sortedConstructors, ([constructorId, driverIds]) => {
-                const constructor = constructors[constructorId]
-                const { name } = constructor
-                const constructorRef = constructorEdges[year][constructorId]
-
-                return (
-                  <div key={constructorId} className={css.constructorContainer}>
-                    <div ref={constructorRef} className={css.constructor}>
-                      {name}
-                    </div>
-                    <div>
-                      {driverIds.map(driverId => {
-                        const driver = drivers[driverId]
-                        const { forename, surname } = driver
-                        return (
-                          <div key={driverId} className={css.driverContainer}>
-                            <div className={css.driver}>{forename} {surname}</div>
-                          </div>
-                        )
-                      })}
-                    </div>
-                    {rendered && (
-                      <Line
-                        to={constructorRef}
-                        from={_.get(constructorEdges[year - 1], constructorId)}
-                      />
-                    )}
-                  </div>
-                )
-              })}
+              {_.map(sortedConstructors, ([constructorId, driverIds]) => (
+                <Constructor
+                  {...this.props}
+                  constructorId={constructorId}
+                  driverIds={driverIds}
+                  constructorEdges={constructorEdges}
+                  drivers={drivers}
+                  rendered={rendered}
+                  key={constructorId}
+                  constructors={constructors}
+                  driverEdges={driverEdges}
+                  year={year}
+                />
+              ))}
             </div>
           )
         })}
+      </div>
+    )
+  }
+}
+
+class Constructor extends React.Component {
+  render() {
+    const {
+      constructors,
+      constructorId,
+      constructorEdges,
+      year,
+      driverIds,
+      driverEdges,
+      rendered,
+      drivers,
+    } = this.props
+
+    const constructor = constructors[constructorId]
+    const { name } = constructor
+
+    const constructorEdge = constructorEdges[constructorId]
+    const constructorRef = constructorEdge[year]
+
+    const yearKeys = Object.keys(constructorEdge)
+    const yearIndex = _.indexOf(yearKeys, year)
+
+    return (
+      <div className={css.constructorContainer}>
+        <div ref={constructorRef} className={css.constructor}>
+          {name}
+        </div>
+        <div>
+          {driverIds.map(driverId => (
+            <Driver
+              driverId={driverId}
+              key={driverId}
+              drivers={drivers}
+              driverEdges={driverEdges}
+              constructorId={constructorId}
+              year={year}
+              rendered={rendered}
+            />
+          ))}
+        </div>
+        {false && rendered && yearIndex > 0 && (
+          <Line
+            to={constructorRef}
+            from={constructorEdge[yearKeys[yearIndex - 1]]}
+          />
+        )}
+      </div>
+    )
+  }
+}
+
+class Driver extends React.Component {
+  render() {
+    const { drivers, driverId, driverEdges, constructorId, year, rendered } = this.props
+
+    const driver = drivers[driverId]
+    const { forename, surname } = driver
+
+    const driverEdge = driverEdges[driverId]
+    const driverRef = driverEdge[year][constructorId]
+
+    const yearKeys = Object.keys(driverEdge)
+    const yearIndex = _.indexOf(yearKeys, year)
+
+    const previousYear = driverEdge[yearKeys[yearIndex - 1]]
+
+    return (
+      <div key={driverId} className={css.driverContainer}>
+        <div className={css.driver} ref={driverRef}>
+          {forename} {surname}
+        </div>
+        {rendered && previousYear && _.map(previousYear, (previousRef, previousConstructorId) => (
+          <Line
+            to={driverRef}
+            key={previousConstructorId}
+            from={previousRef}
+          />
+        ))}
       </div>
     )
   }
